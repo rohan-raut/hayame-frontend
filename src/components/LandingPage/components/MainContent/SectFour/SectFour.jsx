248d47cd229c47b51df8fc294670de31c3bb0cbc
Rohan <rohanraut124@gmail.com>
2023-08-22 00:12:04 +0530

    Updated Landing Page

--- src/components/LandingPage/components/MainContent/SectFour/SectFour.jsx
+++ src/components/LandingPage/components/MainContent/SectFour/SectFour.jsx
@@ -5,19 +5,22 @@ import { sectFourContent } from "../../../../../assets";
 
 const SectFour = () => {
   return (
-    <div className="sect4-div">
+    <div className="sect4-div row justify-content-center m-0">
       <h3 className="sect4-title">Dive deeper into the Businesses we serve</h3>
-      <div className="sect4-card">
+      <div className="col-11 col-sm-11 col-md-10 col-lg-9 sect4-card">
         <img
           src={sectFourContent}
           alt="Table with types of workers"
           className="sect4-table"
         />
-        <Link to="/dashboard">
-          <button className="btn btn-dark mb-4 py-2 px-4 fs-5">
-            Book Now!
-          </button>
-        </Link>
+        <div className="my-3">
+          <Link to="/dashboard">
+            <button className="btn btn-dark book-now-button">
+              Book Now!
+            </button>
+          </Link>
+        </div>
+
       </div>
     </div>
   );
