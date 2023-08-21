a9ba66615cd6d85363a1b3a3c1b29713ccb05a9b
Vrunda Bansode <vrunbansode786@gmail.com>
2023-08-21 21:49:26 +0530

    Book Now button added

--- src/components/DashboardPage/DashboardPage.jsx
+++ src/components/DashboardPage/DashboardPage.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import { NavbarDashboard } from "../LandingPage/components";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Hero from "./components/Hero/Hero";
 // import Footer from "./components/Footer/Footer";
 import "./dashboardPage.css";
+import { useNavigate } from "react-router-dom";
 
 const DashboardPage = () => {
   let user_role = localStorage.getItem("user_role");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkIfLogin = () => {
+      if (!JSON.parse(localStorage.getItem("isLoggedIn"))) {
+        navigate("/login");
+      }
+    };
+
+    checkIfLogin();
+  }, []);
 
   return (
     <div className="dashboard-container-fluid">
