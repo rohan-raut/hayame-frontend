9572a99824aa7f7b75175f92cb1e6f68ff4e7f1b
Rohan <rohanraut124@gmail.com>
2024-03-19 22:38:24 +0530

    Added new video and Images

--- src/components/LandingPage/components/Hero/Hero.jsx
+++ src/components/LandingPage/components/Hero/Hero.jsx
@@ -11,20 +11,24 @@ const Hero = () => {
     };
 
     return (
-
-        <div className="row m-0 align-items-center text-left hero-div py-5">
-            <div className="col-12 col-sm-12 col-md-6 col-lg-6">
-                <h1 className="hero-card-h1">Welcome to Hayame, your on-demand cleaner hiring service. With Hayame, cleanliness is just a click away.</h1>
-                <p className="hero-card-p">
-                Our platform connects you with reliable cleaners tailored to your schedule and needs. 
-                </p>
-                <p className="hero-card-p">
-                Experience hassle-free cleaning with <strong>Hayame</strong> today!
-                </p>
+        <div>
+            <div className="row m-0 justify-content-center align-items-center hero-div">
+                <div className="col-12 col-sm-12 col-md-8 col-lg-8 text-center">
+                    <h1 className="hero-card-h1">Welcome to Hayame, your on-demand cleaner hiring service. With Hayame, cleanliness is just a click away.</h1>
+                    <p className="hero-card-p">
+                        Our platform connects you with reliable cleaners tailored to your schedule and needs.
+                    </p>
+                    <p className="hero-card-p">
+                        Experience hassle-free cleaning with <strong>Hayame</strong> today!
+                    </p>
+                </div>
             </div>
-            <div className="col-12 col-sm-12 col-md-6 col-lg-6 text-center">
-                {/* <img src={team} alt="People working together" /> */}
-                <video src={HayameVideo} autoplay="true" muted className="home-page-video" controls autoPlay></video>
+
+            <div className="row m-0 justify-content-center hero-video-content">
+                <div className="col-12 col-sm-12 col-md-10 col-lg-8 text-center justify-content-center">
+                    {/* <img src={team} alt="People working together" /> */}
+                    <video src={HayameVideo} autoplay="true" muted className="home-page-video" autoPlay></video>
+                </div>
             </div>
         </div>
     );
